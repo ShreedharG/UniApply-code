bd95aeef0b3341ed8649a125d7cc16e602400d04
ShreedharG <shreedhar.goyal@gmail.com>
2026-02-06 04:32:47 +0530

    Enforced file size limits

--- backend/src/middleware/uploadMiddleware.js
+++ backend/src/middleware/uploadMiddleware.js
@@ -21,6 +21,7 @@ const upload = multer({
         console.log("Multer File Filter:", { fieldname: file.fieldname, mimetype: file.mimetype });
         checkFileType(file, cb);
     },
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
 });
 
 export default upload;
